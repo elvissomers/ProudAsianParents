fcae621417531866ca8b63fca7807c7c85987fbf
elvissomers <E.M.somers@student.tudelft.nl>
2017-12-01 12:18:28 +0100

    Fixed edit button

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -34,6 +34,7 @@ var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:x
 var editSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 129"> <g> <g> <path d="m119.2,114.3h-109.4c-2.3,0-4.1,1.9-4.1,4.1s1.9,4.1 4.1,4.1h109.5c2.3,0 4.1-1.9 4.1-4.1s-1.9-4.1-4.2-4.1z"/> <path d="m5.7,78l-.1,19.5c0,1.1 0.4,2.2 1.2,3 0.8,0.8 1.8,1.2 2.9,1.2l19.4-.1c1.1,0 2.1-0.4 2.9-1.2l67-67c1.6-1.6 1.6-4.2 0-5.9l-19.2-19.4c-1.6-1.6-4.2-1.6-5.9-1.77636e-15l-13.4,13.5-53.6,53.5c-0.7,0.8-1.2,1.8-1.2,2.9zm71.2-61.1l13.5,13.5-7.6,7.6-13.5-13.5 7.6-7.6zm-62.9,62.9l49.4-49.4 13.5,13.5-49.4,49.3-13.6,.1 .1-13.5z"/> </g> </g></svg>';
 
 renderList();
+console.log("1");
 
 document.getElementById('add').addEventListener('click', function () {
     var value = document.getElementById('item').value;
@@ -58,15 +59,22 @@ document.getElementById('item').addEventListener('keydown', function (e) {
     }
 });
 
+document.getElementById('item2').addEventListener('keydown', function (e) {
+    var value = this.value
+    if (e.code === 'Enter' && value) {
+        addItem2(value);
+    }
+});
+
 function addItem(value) {
-    addItemToDOM(value);
+    addItemToDOM(value,1,1);
     document.getElementById('item').value = '';
     data.todo.push(value);
     dataObjectUpdate();
 }
 
 function addItem2(value) {
-    addItemToDOM(value, 3);
+    addItemToDOM(value, 3,1);
     document.getElementById('item2').value = '';
     data.nottodo.push(value);
     dataObjectUpdate();
@@ -83,22 +91,22 @@ function renderList() {
 
     for (var k = 0; k < data.nottodo.length; k++) {
         var value = data.nottodo[k];
-        addItemToDOM(value, 3);
+        addItemToDOM(value, 3,1);
     }
 
     for (var l = 0; l < data.notdone.length; l++) {
         var value = data.notdone[l];
-        addItemToDOM(value, 4);
+        addItemToDOM(value, 4,1);
     }
 
     for (var i = 0; i < data.todo.length; i++) {
         var value = data.todo[i];
-        addItemToDOM(value, 1);
+        addItemToDOM(value, 1,1);
     }
 
     for (var j = 0; j < data.completed.length; j++) {
         var value = data.completed[j];
-        addItemToDOM(value, 2);
+        addItemToDOM(value, 2,1);
     }
 
 }
@@ -110,7 +118,7 @@ function renderList() {
  * @param {*} type 1 = to-do; 2 = done; 3 = not-to-do (bad habit);
  * 4 = not done (bad habit)
  */
-function addItemToDOM(text, type) {
+function addItemToDOM(text, type, index) {
     var list = document.getElementById('todo');
 
     switch (type) {
@@ -154,6 +162,7 @@ function addItemToDOM(text, type) {
     edit.classList.add('edit');
     edit.innterHTML = editSVG;
     edit.addEventListener('click', editItem);
+    edit.addEventListener('click', removeItem);
 
 
     buttons.appendChild(remove);
@@ -161,7 +170,7 @@ function addItemToDOM(text, type) {
     buttons.appendChild(edit);
     item.appendChild(buttons);
 
-    list.insertBefore(item, list.childNodes[0]);
+    list.insertBefore(item, list.childNodes[index]);
 }
 
 function dataObjectUpdate() {
@@ -245,19 +254,50 @@ function completeItem() {
 function editItem() {
     var item = this.parentNode.parentNode;
     // var text = item.innerText;
+    
     var parent = item.parentNode;
+    // console.log(parent.indexOf(item));
     var parentID = parent.id;
     var value;
+    var oldvalue = item.innerText;
+    var index;
+
+    for(var i = 0; i < parent.childNodes.length; i++){
+        if(parent.childNodes[i] == item){
+            index = i;
+        }
+    }
+
+    console.log(index);
 
     if (parentID === 'todo' || parentID === 'completed') {
         value = document.getElementById('item').value;
+        document.getElementById('item').value = '';
     } else{
         value = document.getElementById('item2').value;
+        document.getElementById('item2').value = '';
     }
+    var target;
 
     if(value){
-        item.innerText = value;
+        // item.innerText = value;
+        if (parentID === 'todo') {
+            target = 1;
+            data.todo.splice((data.todo.length-index),0,value);
+        } else if (parentID === 'nottodo') {
+            target = 3;
+            data.nottodo.splice((data.nottodo.length-index),0,value);
+        } else if (parentID === 'notdone') {
+            target = 4;
+            data.notdone.splice((data.notdone.length-index),0,value);
+        } else {
+            target = 2;
+            data.completed.splice((data.completed.length-index),0,value);
+        }
+    
+        // item.removeItem();
+        addItemToDOM(value, target, index);
+        dataObjectUpdate();
     }
 
-    dataObjectUpdate();
 }
